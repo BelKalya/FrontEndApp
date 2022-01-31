e1f55d8a377617e68f1c8db341aaae5bc7bbf1c5
Belk <andriiperervin@gmail.com>
2022-02-01 01:28:39 +0200

    Cool behavior for login, if user is authed it will show you are already logged in component and will redirect him to the home afterwards.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Form, Formik} from "formik";
 import {MeDocument, MeQuery, useLoginMutation} from "../generated/graphql";
-import {withApollo} from "../utils/withApollo";
-import {Box, Button, Heading} from "@chakra-ui/react";
+import {createClient, withApollo} from "../utils/withApollo";
+import {Box, Button, Heading, Text} from "@chakra-ui/react";
 import FormInput from "../components/FormInput";
 import {Layout} from "../components/Layout";
 import {useRouter} from "next/router";
 import {INVALID_EMAIL, INVALID_PASSWORD, REQUIRED} from "../utils/validation-errors";
 import {EMAIL_VALIDATION_REGEXP, PASSWORD_VALIDATION_REGEXP} from "../utils/constants";
+import {InferGetServerSidePropsType} from "next";
+import {NextChakraLink} from "../components/NextChakraLink";
 
-const Login = () => {
+export async function getServerSideProps(context) {
+    const client = createClient(context);
+    const { data } = await client.query<MeQuery>({query: MeDocument});
+    return {
+        props: {
+            user: data.me
+        }
+    }
+}
+
+const Login = ({user}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter();
     const [login] = useLoginMutation();
+    useEffect(() => {
+        if (user) {
+            setTimeout(() => {router.replace('/')}, 3000);
+        }
+    });
+
+    if (user) {
+        return (
+            <Layout title="Login">
+                <Heading mb={8}>You are already logged in</Heading>
+                <Box>
+                    <Text fontSize={"lg"} lineHeight={"tall"} >
+                        You will be redirected to the homepage shortly
+                    </Text>
+                    <Text fontSize={"lg"} lineHeight={"tall"}>
+                        If redirect didn't happen in 5 seconds please click this <NextChakraLink fontWeight={"bold"} href="/">Link</NextChakraLink>
+                    </Text>
+                </Box>
+            </Layout>
+        )
+    }
     return (
         <Layout title="Login">
             <Box maxW={600} m={"0 auto"}>
                 <Heading size={"xl"} mb={8}>Login</Heading>
                 <Formik
-                    initialValues={{ email: '', password: '' }}
+                    initialValues={{email: '', password: ''}}
                     validate={(values) => {
                         const errors: { email?: string, password?: string } = {};
                         if (!values.email) {
@@ -32,10 +65,10 @@ const Login = () => {
                         }
                         return errors;
                     }}
-                    onSubmit={async (values, { setErrors }) => {
+                    onSubmit={async (values, {setErrors}) => {
                         const response = await login({
                             variables: values,
-                            update: (cache, { data }) => {
+                            update: (cache, {data}) => {
                                 cache.writeQuery<MeQuery>({
                                     query: MeDocument,
                                     data: {
@@ -47,7 +80,7 @@ const Login = () => {
                         });
                         if (response.data?.login.errors) {
                             setErrors(Object.fromEntries(response.data.login.errors.map((e) => {
-                                return [e.field,e.message];
+                                return [e.field, e.message];
                             })));
                         } else if (response.data?.login.user) {
                             if (typeof router.query.forward === "string") {
@@ -58,17 +91,20 @@ const Login = () => {
                         }
                     }}
                 >
-                    {({ values,
+                    {({
+                          values,
                           errors,
                           handleChange,
                           handleBlur,
                           touched,
-                          isSubmitting }) => (
+                          isSubmitting
+                      }) => (
                         <Form>
                             <FormInput name="email" label="Email" handleChange={handleChange} handleBlur={handleBlur}
-                                       value={values.email} error={errors.email} touched={touched.email} />
-                            <FormInput name="password" label="Password" handleChange={handleChange} handleBlur={handleBlur}
-                                       value={values.password} error={errors.password} touched={touched.password} />
+                                       value={values.email} error={errors.email} touched={touched.email}/>
+                            <FormInput name="password" label="Password" handleChange={handleChange}
+                                       handleBlur={handleBlur}
+                                       value={values.password} error={errors.password} touched={touched.password}/>
                             <Button mt={4} type="submit" disabled={isSubmitting} colorScheme="teal" size="md">
                                 Submit
                             </Button>
--- utils/withApollo.ts
+++ utils/withApollo.ts
@@ -2,7 +2,7 @@ import {createWithApollo} from "./createWithApollo";
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 import {NextPageContext} from "next";
 
-const createClient = (ctx: NextPageContext) =>
+export const createClient = (ctx: NextPageContext) =>
     new ApolloClient({
         uri: 'http://localhost:4000/graphql',
         credentials: "include",
